9dd255daadde4f2f058f5f59d7750f014dad8168
itagawa_tomoya <gawawa.developer@gmail.com>
2017-09-04 20:04:33 +0900

    modify for production

--- app.py
+++ app.py
@@ -3,15 +3,16 @@
 from flask import Flask, render_template, request
 import simulator
 import requests
+import os
 
 app = Flask(__name__)
 
-url = "https://api.heroku.com/apps/revolving-credit-simulation/config-vars"
-headers = {"Accept": "application/vnd.heroku+json; version=3"}
-response = requests.get(url, headers=headers).json()
-
-# app.config["KEEN_PROJECT_ID"] = response["KEEN_PROJECT_ID"]
-# app.config["KEEN_WRITE_KEY"] = response["KEEN_WRITE_KEY"]
+if os.environ.get("KEEN_PROJECT_ID") is not None and os.environ.get("KEEN_WRITE_KEY") is not None:
+    app.config["DEBUG"] = False
+    app.config["KEEN_PROJECT_ID"] = os.environ["KEEN_PROJECT_ID"]
+    app.config["KEEN_WRITE_KEY"] = os.environ["KEEN_WRITE_KEY"]
+else:
+    app.config["DEBUG"] = True
 
 @app.route("/")
 def index():
@@ -22,7 +23,7 @@ def result():
     borrowing = request.form.get("borrowing", type=int)
     repayment = request.form.get("repayment", type=int)
     if borrowing is None or repayment is None:
-        return render_template("error.html", error=response)
+        return render_template("error.html", error="入力欄に数字を入力してください。")
     else:
         result, result_list = simulator.simulate(borrowing, repayment)
         return render_template("result.html", result=result, result_list=result_list)
