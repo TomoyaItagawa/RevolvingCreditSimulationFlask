b5567f99b4347e7e881916f7b7be9b2bbb8c5336
itagawa_tomoya <gawawa.developer@gmail.com>
2017-09-04 19:23:52 +0900

    on the way

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ url = "https://api.heroku.com/apps/revolving-credit-simulation/config-vars"
 headers = {"Accept": "application/vnd.heroku+json; version=3"}
 response = requests.get(url, headers=headers).json()
 
-app.config["KEEN_PROJECT_ID"] = response["KEEN_PROJECT_ID"]
-app.config["KEEN_WRITE_KEY"] = response["KEEN_WRITE_KEY"]
+# app.config["KEEN_PROJECT_ID"] = response["KEEN_PROJECT_ID"]
+# app.config["KEEN_WRITE_KEY"] = response["KEEN_WRITE_KEY"]
 
 @app.route("/")
 def index():
@@ -22,7 +22,7 @@ def result():
     borrowing = request.form.get("borrowing", type=int)
     repayment = request.form.get("repayment", type=int)
     if borrowing is None or repayment is None:
-        return render_template("error.html", error="入力欄に数字を入力してください。")
+        return render_template("error.html", error=response)
     else:
         result, result_list = simulator.simulate(borrowing, repayment)
         return render_template("result.html", result=result, result_list=result_list)
