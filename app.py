d3bc7421c1f3157a33a11f692b1f1d67a0fd47ec
itagawa_tomoya <gawawa.developer@gmail.com>
2017-09-12 21:49:17 +0900

    add payment_type, simulate_fixed_rate

--- app.py
+++ app.py
@@ -20,13 +20,20 @@ def index():
 
 @app.route("/result", methods=["GET", "POST"])
 def result():
+    payment_type = request.form.get("payment_type", type=int)
     borrowing = request.form.get("borrowing", type=int)
     repayment = request.form.get("repayment", type=int)
     if borrowing is None or repayment is None:
         return render_template("error.html", error="入力欄に数字を入力してください。")
     else:
-        result, result_list = simulator.simulate(borrowing, repayment)
+        result, result_list = simulator.simulate(payment_type, borrowing, repayment)
+        if result is None or result_list is None:
+            return render_template("error.html", error="シミュレーションに失敗しました。")
         return render_template("result.html", result=result, result_list=result_list)
+
+@app.route("/explanation")
+def explanation():
+    return render_template("explanation.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- simulator.py
+++ simulator.py
@@ -1,8 +1,57 @@
 # -*- coding: utf-8 -*-
 
 import math
+from enum import Enum
 
-def simulate(borrowing, repayment):
+class PaymentType(Enum):
+    FIXED_RATE = 1
+    FIXED_CAPITAL = 2
+
+rate = 0.15
+
+def simulate(payment_type, borrowing, repayment):
+    if payment_type == PaymentType.FIXED_RATE.value:
+        return __simulate_fixed_rate(borrowing, repayment)
+    elif payment_type == PaymentType.FIXED_CAPITAL.value:
+        return __simulate_fixed_capital(borrowing, repayment)
+    else:
+        return (None, None)
+
+def __simulate_fixed_rate(borrowing, repayment):
+    month = 0
+    payment = 0
+    capital = 0
+    interest = 0
+    balance = borrowing
+    interest_amount = 0
+    payment_amount = 0
+    result = {}
+    result_list = []
+
+    i = 0
+    while(balance > 0):
+        interest = int(balance * rate / 12)
+        if balance + interest > repayment:
+            payment = repayment
+        else:
+            payment = balance + interest
+
+        capital = payment - interest
+        balance -= capital
+        result_list.append([i + 1, payment, capital, interest, balance])
+
+        interest_amount += interest
+        payment_amount += payment
+        month += 1
+        i += 1
+
+    result["interest_amount"] = interest_amount
+    result["payment_amount"] = payment_amount
+    result["month"] = month
+
+    return (result, result_list)
+
+def __simulate_fixed_capital(borrowing, repayment):
     month = 0
     payment = 0
     capital = 0
@@ -22,7 +71,7 @@ def simulate(borrowing, repayment):
         else:
             capital = repayment
 
-        interest = int(balance * 0.15 / 12)
+        interest = int(balance * rate / 12)
         payment = capital + interest
         balance -= capital
 
